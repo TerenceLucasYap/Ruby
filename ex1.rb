8e4da1db1c9931bac2473f3b51340358c8b63f45
Papagoat <terence@terences-mbp.home.gateway>
2013-10-28 22:24:05 +1100

    Ruby 3

--- ex1.rb
+++ ex1.rb
@@ -6,3 +6,4 @@ puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
 puts 'I "said" do not touch this please.'
+puts 'I "said" do not touch this please please please.'
